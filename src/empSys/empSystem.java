bfe8a4fb37a6d50ea295608ca950ef748609e0bc
beni2412 <hardaman97@gmail.com>
2020-09-27 23:28:10 +0530

    [Hardaman] Added: Ability to compute wage for multiple companies

--- src/empSys/empSystem.java
+++ src/empSys/empSystem.java
@@ -1,36 +1,40 @@
 package empSys;
-
+import java.util.Random;
  public class empSystem {
+	 public static final int IS_FULL_TIME = 1;
+		public static final int IS_PART_TIME = 2;
 
-	 	public static final int IS_PART_TIME =1;
-		public static final int IS_FULL_TIME =2;
-		public static final int EMP_RATE_PER_HOUR = 20;
-		public static final int NUM_OF_WORKING_DAYS = 20;
-		public static final int MAX_HRS_IN_MONTH = 100;
+		public static int computeEmpWage(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth) {
+		int empHrs = 0, totalEmpHrs = 0, totalEmpWage = 0, totalWorkingDays = 0;
+
+		while (totalEmpHrs <= maxHoursPerMonth && totalWorkingDays < numOfWorkingDays) {
+		totalWorkingDays++;
+
+		Random r = new Random();
+		int empCheck = r.nextInt(3);
+
+		switch (empCheck) {
+		case IS_FULL_TIME:
+		 empHrs = 8;
+		 break;
+		case IS_PART_TIME:
+		 empHrs = 4;
+		 break;
+		default:
+		 empHrs = 0;
+
+		}
+		totalEmpHrs += empHrs;
+		System.out.println("Day : " + totalWorkingDays + "empHrs : " + empHrs);
+		}
+		totalEmpWage = totalEmpHrs * empRatePerHour;
+		System.out.println("Total Emp Wage for Company: " +company+" is: "+ totalEmpWage);
+		return totalEmpWage;
+		}
 		
 		public static void main(String[] args) {
-			int empHrs = 0;
-			int totalWorkingDays = 0;
-			int totalEmpHrs = 0;
-			
-			while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
-				totalWorkingDays++;
-				int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-				switch (empCheck) {
-				case IS_PART_TIME:
-					empHrs = 4;
-					break;
-				case IS_FULL_TIME:
-					empHrs = 8;
-					break;
-				default:
-					empHrs = 0;
-				}
-				totalEmpHrs +=empHrs;
-				System.out.println("DAY#: " + totalWorkingDays + "Emp Hr: "+ empHrs);
-			}
-			int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
-			System.out.println("Total Emp Wage: " + totalEmpWage);
+	     computeEmpWage("Reliance", 15, 25 , 100);
+	     computeEmpWage("BigBazar", 20, 27 , 90);
 		}
  }
 
